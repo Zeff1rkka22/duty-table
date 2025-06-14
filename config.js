a8b02c9dac54e6c496f09a1f607f817bfcd1dd7e
Zeff1rkka22 <lolkkeknun@gmail.com>
2025-06-14 15:53:17 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,17 @@
+import firebase from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBcR5RnGuTCMUXuGTbVqdj_1pcJ1n77kTk",
-    authDomain: "duty-table-c6d39.firebaseapp.com",
-    projectId: "duty-table-c6d39",
-    storageBucket: "duty-table-c6d39.firebasestorage.app",
-    messagingSenderId: "903907682283",
-    appId: "1:903907682283:web:7977a2450b47260b3b5bae"
+  apiKey: "AIzaSyBcR5RnGuTCMUXuGTbVqdj_1pcJ1n77kTk",
+  authDomain: "duty-table-c6d39.firebaseapp.com",
+  projectId: "duty-table-c6d39",
+  storageBucket: "duty-table-c6d39.appspot.com",
+  messagingSenderId: "411618930133",
+  appId: "1:411618930133:web:e2f36c58e848ebf5521fa5"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const firestore = firebase.firestore();
